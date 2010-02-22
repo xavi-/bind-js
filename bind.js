acf1251b3ef0bbbc6db0151e7a8716a4caa5dc84
Xavi <xavi.rmz@gmail.com>
2010-02-22 08:20:36 -0500

    Added support for default values and functions.

--- bind.js
+++ bind.js
@@ -1,12 +1,28 @@
-(function(context) {
+(function(context, undefined) {
     var evt = require("events");
     var fs = require("fs");
+    
+    var toString = Object.prototype.toString;
     
     context.bind = function bind(file, context) {
         var promise = new evt.Promise();
         
-        fs.readFile(file, "utf8").addCallback(function(data) {
-            var bound = data.replace(/{{(.+?)}}/, function(match, item) { return context[item]; });
+        fs.readFile(file).addCallback(function(data) {
+            var bound = data.replace(/{{(.+?)}}/g, function(match, item) {
+                var split = item.match(/(.+?)\s*?:(.+)/) || [];
+                var key = split[1] || item, defVal = split[2] || "";
+                var val = context[key];
+                
+                if(val == undefined) { return defVal; }
+                
+                if(toString.call(val) === "[object String]") { return val; }
+                
+                if(toString.call(val) === "[object Function]") { return val(defVal); }
+                
+                if(toString.call(val) === "[object Number]") { return val.toString(); }
+                
+                if(toString.call(val) === "[object Boolean]") { return val.toString(); }
+            });
             promise.emitSuccess(bound);
         });
         
