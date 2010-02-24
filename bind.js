159876b365ad252450e8dd480143e17d3e5a82e1
Xavi <xavi.rmz@gmail.com>
2010-02-24 07:34:57 -0500

    Moved binding logic to separate function.

--- bind.js
+++ bind.js
@@ -4,25 +4,27 @@
     
     var toString = Object.prototype.toString;
     
-    context.bind = function bind(file, context) {
+    function binder(binds, item) {
+        var split = item.match(/\s*(.+?)\s*:(.+)/) || [];
+        var key = split[1] || item, defVal = split[2] || "";
+        var val = binds[key];
+        sys.puts("key: " + key + "; val: " + val + "; defVal: " + defVal);
+        if(val == undefined) { return defVal; }
+        
+        if(toString.call(val) === "[object String]") { return val; }
+        
+        if(toString.call(val) === "[object Function]") { return val(defVal); }
+        
+        if(toString.call(val) === "[object Number]") { return val.toString(); }
+        
+        if(toString.call(val) === "[object Boolean]") { return val.toString(); }
+    }
+    
+    context.bind = function bind(file, binds) {
         var promise = new evt.Promise();
         
         fs.readFile(file).addCallback(function(data) {
-            var bound = data.replace(/{{(.+?)}}/g, function(match, item) {
-                var split = item.match(/\s*(.+?)\s*:(.+)/) || [];
-                var key = split[1] || item, defVal = split[2] || "";
-                var val = context[key];
-                
-                if(val == undefined) { return defVal; }
-                
-                if(toString.call(val) === "[object String]") { return val; }
-                
-                if(toString.call(val) === "[object Function]") { return val(defVal); }
-                
-                if(toString.call(val) === "[object Number]") { return val.toString(); }
-                
-                if(toString.call(val) === "[object Boolean]") { return val.toString(); }
-            });
+            var bound = data.replace(/{{(.+?)}}/g, function(_, item) { return binder(binds, item); });
             promise.emitSuccess(bound);
         });
         
